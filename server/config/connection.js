ba2bf1af2c6f38c5af9c7fd6142fd5c0f0ff6350
csabbah <csabbah@ryerson.ca>
2022-11-14 14:38:50 -0500

    Added a custom dbName to mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  dbName: "devlp",
 });
 
 module.exports = mongoose.connection;
