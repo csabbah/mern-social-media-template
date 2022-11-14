7992ae521280ee8fd147c164110a6b85987697bf
csabbah <csabbah@ryerson.ca>
2022-11-14 15:26:18 -0500

    Misc commit - Added a list of APIs to fetch data from (for future use)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from "react";
 
 import Auth from "../utils/auth";
+import { test } from "../utils/API";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  const returnData = async () => {
+    const res = await test();
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      Home Page
+      <button onClick={() => returnData()}>Search</button>
+    </div>
+  );
 };
 
 export default Home;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,57 +1,38 @@
-// route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch('/api/users/me', {
+export const test = async () => {
+  const options = {
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
+      "X-RapidAPI-Key": "ffae5646afmshec63d61fbd07b2fp17ee73jsn3371d91d22c0",
+      "X-RapidAPI-Host": "anime-db.p.rapidapi.com",
     },
-  });
+  };
+  return fetch(
+    "https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=Fullmetal&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc",
+    options
+  );
 };
 
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
+// IMPORTANT NOTE
+// Realistically, we only need to use certain APIs once to upload to the database
+// I.e. We only need to run the Geography API once and then save all the data
+// However, with news, that updates daily, so we cannot do that
 
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-};
+// Articles (Medium - Expensive) / Free = 150 requests/month, no request frequency limit
+// 2 requests are needed > Get Top Feeds > Take a random Article ID > Get Article's Content
+// https://rapidapi.com/nishujain199719-vgIfuFHZxVZ/api/medium2
 
-// save book data for a logged in user
-export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(bookData),
-  });
-};
+// SUBSCRIBED - Geography / Free = 1 request/second & 100 requests/day
+// https://rapidapi.com/natkapral/api/countries-cities/
 
-// remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
-};
+// SUBSCRIBED - Vocabulary / Free = 2,500 requests/day, no request frequency limit
+// https://rapidapi.com/dpventures/api/wordsapi/pricing
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
-};
+// SUBSCRIBED - Facts / No premium option, unlimited requests, no request frequency limit
+// https://rapidapi.com/brianweidl/api/random-facts4/
+
+// SUBSCRIBED - Articles (Good one) - Free = 50 requests/month, no request frequency limit
+// Users can input their city to return local news
+// https://rapidapi.com/letscrape-6bRBa3QguO5/api/real-time-news-data/pricing
+
+// SUBSCRIBED - Famous quotes - Free = 1,000 requests/month, no request frequency limit
+// https://rapidapi.com/saicoder/api/famous-quotes4/
